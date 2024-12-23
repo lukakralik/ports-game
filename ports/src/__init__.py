87127b43918474cb950294df87fd348f8a11b28f
Luka Králík <luka.kralik@gmail.com>
2024-12-23 22:06:33 +0100

    logging to a file

--- ports/src/__init__.py
+++ ports/src/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 login = LoginManager(app)
@@ -11,5 +14,19 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view='login' # url_for()
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler('logs/ports.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Ports game')
+
 
 from src import routes, models, errors
